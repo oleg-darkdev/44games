5bbe211b4da5c8070b3fc0248a9ced3c875d8dd3
DarkDev <darkdev_brand@riseup.net>
2023-03-04 23:21:53 +0200

    alfa v.

---
+++ src/features/ui/FiltresItCategory.svelte
@@ -0,0 +1,32 @@
+<script>
+
+</script>
+
+<div class="phone-1 artboard bg-gray-900 p-4 rounded-lg m-4 fixed relative">
+	<div class="flex flex-col">
+		<div class="form-control">
+			<label class="label cursor-pointer">
+				<span class="label-text">Remember me</span>
+				<input type="checkbox" class="toggle-secondary toggle toggle-lg" checked />
+			</label>
+		</div>
+		<div class="form-control">
+			<label class="label cursor-pointer">
+				<span class="label-text">Remember me</span>
+				<input type="checkbox" class="toggle-secondary toggle toggle-lg" checked />
+			</label>
+		</div>
+		<div class="form-control">
+			<label class="label cursor-pointer">
+				<span class="label-text">Remember me</span>
+				<input type="checkbox" class="toggle-secondary toggle toggle-lg" checked />
+			</label>
+		</div>
+		<div class="form-control">
+			<label class="label cursor-pointer">
+				<span class="label-text">Remember me</span>
+				<input type="checkbox" class="toggle-secondary toggle toggle-lg" checked />
+			</label>
+		</div>
+	</div>
+</div>
