7aaf3994cb82b8e0199e92ad0ea937886d858a41
DarkDev <darkdev_brand@riseup.net>
2023-03-04 23:21:20 +0200

    upd padding, fix for laptop

--- src/widgets/ui/GamesCategorySection.svelte
+++ src/widgets/ui/GamesCategorySection.svelte
@@ -5,8 +5,8 @@
 	export let games, categoryTitle;
 </script>
 
-<section class="m-12 flex h-auto flex-col  rounded-xl bg-gray-900 p-6">
-	<h2 class="mb-4 w-full text-left text-5xl text-gray-200">{categoryTitle}</h2>
+<div class="flex h-auto flex-col  rounded-xl bg-gray-900 p-6 lg:w-9/12 xl:w-9/12 md:w-9/12 w-full">
+	<h2 class="mb-4 pl-8 w-full text-left text-5xl text-gray-200">{categoryTitle}</h2>
     	<Carousel
 		games={[].concat(convertObjectToArray(games))}
 		controlColor={'white'}
@@ -15,4 +15,4 @@
 		autoplaySpeed={3000}
 	>
   </Carousel>
-</section>
+</div>
