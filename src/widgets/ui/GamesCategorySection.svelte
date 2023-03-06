df4c82588fa423d71644119e981a18419d070ddb
DarkDev <darkdev_brand@riseup.net>
2023-03-06 14:32:35 +0200

    fix width for md screens

--- src/widgets/ui/GamesCategorySection.svelte
+++ src/widgets/ui/GamesCategorySection.svelte
@@ -4,7 +4,7 @@
 	export let games, categoryTitle;
 </script>
 
-<div class="flex h-auto flex-col  rounded-xl bg-gray-900 p-6 lg:w-9/12 xl:w-9/12 md:w-9/12 w-full">
+<div class="flex h-auto flex-col  rounded-xl bg-gray-900 p-6 lg:w-9/12 xl:w-9/12 md:w-8/12 w-full">
 	<h2 class="mb-4 pl-8 w-full text-left text-5xl text-gray-200">{categoryTitle}</h2>
     	<Carousel
 		{games}
