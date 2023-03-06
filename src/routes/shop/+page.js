06c28b99cc6618e4a5b03c06d3005c51969f6e61
DarkDev <darkdev_brand@riseup.net>
2023-03-06 14:31:22 +0200

    refactoring, add sorted by categories

--- src/routes/shop/+page.js
+++ src/routes/shop/+page.js
@@ -1,18 +1,11 @@
-import historicalGames from './games/categories/historicalGames';
-import civicActivismGames from './games/categories/civicActivismGames';
-import artGames from './games/categories/artGames';
-import itGames from './games/categories/itGames';
+import allGamesList from './games/AllGamesList';
 import { client } from './commerce';
-
-const allGamesList = [].concat(historicalGames, civicActivismGames, artGames, itGames);
 
 // commerceJs
 const merchant = await client.merchants.about(),
 	cart = await client.cart.retrieve(), // store
 	categoriesList = await client.categories.list(),
 	{ data: products } = await client.products.list();
-
-let gameList = [];
 
 const mergeGameData = (game, commerceJsProduct) =>
 	Object.assign(game, {
@@ -23,27 +16,50 @@ const mergeGameData = (game, commerceJsProduct) =>
 			thankYouUrl: commerceJsProduct.thank_you_url,
 			conditionals: commerceJsProduct.conditionals,
 			collects: commerceJsProduct.collects,
-			checkoutUrl: commerceJsProduct.checkout_url
+			checkoutUrl: commerceJsProduct.checkout_url,
+			categories: commerceJsProduct.categories
 		}
 	});
 
-const findGame = () => {
-	let foundedGame;
+const mergeCommerceJsAndLocalGameData = () => {
+	let foundedGame, resultGameList = [];
 
 	products.forEach((product) => {
 		foundedGame = allGamesList.find((game) => game.promo.idCommerceJs === product.id);
-		gameList.push(mergeGameData(foundedGame, product));
+		resultGameList.push(mergeGameData(foundedGame, product));
 	});
+
+	return resultGameList;
 };
 
-export async function load({ params }) {
-	findGame();
+let filredGames = (category) =>
+	gameList.filter((element) =>
+		element.commerceJs.categories.some((gameCategory) => gameCategory.slug === category)
+	);
 
+const gameList = mergeCommerceJsAndLocalGameData();
+const historyGames = filredGames('historical'),
+	activismGames = filredGames('social-activism'),
+	artGames = filredGames('art'),
+	itGames = filredGames('it');
+
+const exportAllGamesList = [
+	{ category: 'Historical', data: historyGames },
+	{ category: 'Art', data: artGames },
+	{ category: 'IT', data: itGames },
+	{ category: 'Social activism', data: activismGames }
+];
+
+export async function load({ params }) {
 	return {
-		allGamesList: gameList,
+		allGamesList: exportAllGamesList,
+		historyGames: historyGames,
+		activismGames: activismGames,
+		artGames: artGames,
+		itGames: itGames,
+
 		merchant: merchant,
 		cart: cart,
-		categories: categoriesList,
-		gameList: gameList
+		categories: categoriesList
 	};
 }
