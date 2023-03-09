9b36c3ed93a6d626af628e34e2771685a2231cda
DarkDev <darkdev_brand@riseup.net>
2023-03-09 22:40:44 +0200

    refactoring header

---
+++ src/entities/ui/BurgerMenu.svelte
@@ -0,0 +1,25 @@
+<div class="dropdown">
+	<label tabindex="0" class="btn-ghost btn-circle btn">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-5 w-5"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+			><path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M4 6h16M4 12h16M4 18h7"
+			/></svg
+		>
+	</label>
+	<ul
+		tabindex="0"
+		class="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
+	>
+		<li><a href="/">Homepage</a></li>
+		<li><a href="/shop">Portfolio</a></li>
+		<li><a href="/team">Team</a></li>
+	</ul>
+</div>
---
+++ src/entities/ui/UserMenuBtn.svelte
@@ -0,0 +1,20 @@
+<div class="dropdown dropdown-end">
+	<label tabindex="0" class="btn-ghost btn-circle avatar btn">
+		<div class="w-10 rounded-full">
+			<img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+		</div>
+	</label>
+	<ul
+		tabindex="0"
+		class="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-gray-900 p-2 shadow"
+	>
+		<li class="hover:bg-info hover:text-gray-900">
+			<a class="justify-between">
+				Profile
+				<span class="badge">New</span>
+			</a>
+		</li>
+		<li class="hover:bg-info hover:text-gray-900"><a>Settings</a></li>
+		<li class="hover:bg-info hover:text-gray-900"><a>Logout</a></li>
+	</ul>
+</div>
