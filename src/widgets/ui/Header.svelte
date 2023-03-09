d5acd9bb103734dabb7fb43c6c6db30ae4009e4b
DarkDev <darkdev_brand@riseup.net>
2023-03-09 22:40:15 +0200

    refactoring

--- src/widgets/ui/Header.svelte
+++ src/widgets/ui/Header.svelte
@@ -1,31 +1,22 @@
+<script>
+	import { CartHeaderBtn } from '../../features';
+	import { UserMenuBtn, BurgerMenu } from '../../entities';
+
+	const cartTotal = {
+		items: 9, // store length
+		price: 1350
+	};
+
+	export let cartData;
+	// console.log(cartData);
+
+
+</script>
+
 <header>
 	<div class="navbar bg-gray-800">
 		<div class="navbar-start">
-			<div class="dropdown">
-				<label tabindex="0" class="btn-ghost btn-circle btn">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="h-5 w-5"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-						><path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M4 6h16M4 12h16M4 18h7"
-						/></svg
-					>
-				</label>
-				<ul
-					tabindex="0"
-					class="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
-				>
-					<li><a href="/">Homepage</a></li>
-					<li><a href="/shop">Portfolio</a></li>
-					<li><a href="/team">Team</a></li>
-				</ul>
-			</div>
+			<BurgerMenu />
 		</div>
 		<div class="navbar-center">
 			<a class="btn-ghost btn text-xl normal-case">
@@ -36,55 +27,8 @@
 		</div>
 		<div class="navbar-end">
 			<div class="flex-none">
-				<div class="dropdown-end dropdown">
-					<label tabindex="0" class="btn-ghost btn-circle btn">
-						<div class="indicator">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="h-5 w-5"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-								><path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-								/></svg
-							>
-							<span class="badge badge-sm indicator-item">8</span>
-						</div>
-					</label>
-					<div tabindex="0" class="card dropdown-content card-compact mt-3 w-52 bg-base-100 shadow">
-						<div class="card-body">
-							<span class="text-lg font-bold">8 Items</span>
-							<span class="text-info">Subtotal: $999</span>
-							<div class="card-actions">
-								<button class="btn-primary btn-block btn">View cart</button>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="dropdown-end dropdown">
-					<label tabindex="0" class="btn-ghost btn-circle avatar btn">
-						<div class="w-10 rounded-full">
-							<img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-						</div>
-					</label>
-					<ul
-						tabindex="0"
-						class="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
-					>
-						<li>
-							<a class="justify-between">
-								Profile
-								<span class="badge">New</span>
-							</a>
-						</li>
-						<li><a>Settings</a></li>
-						<li><a>Logout</a></li>
-					</ul>
-				</div>
+				<CartHeaderBtn {cartData} />
+				<UserMenuBtn />
 			</div>
 		</div>
 	</div>
