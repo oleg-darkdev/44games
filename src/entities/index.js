a97a1c46034c38fa62966b0bbf00ab0612d2bfd5
DarkDev <darkdev_brand@riseup.net>
2023-03-10 00:01:46 +0200

    refactoring

--- src/entities/index.js
+++ src/entities/index.js
@@ -1,6 +1,21 @@
 import AddBacketBtn from './ui/AddBacketBtn.svelte';
-import BurgerMenu from './ui/BurgerMenu.svelte';
-import CartInfo from './ui/CartInfo.svelte';
-import UserMenuBtn from './ui/UserMenuBtn.svelte';
 
-export { AddBacketBtn, BurgerMenu, CartInfo, UserMenuBtn };
+import BurgerMenu from './ui/header/BurgerMenu.svelte';
+import UserMenuBtn from './ui/header/UserMenuBtn.svelte';
+import CartInfo from './ui/header/CartInfo.svelte';
+import CartSummary from './ui/cart/Summary.svelte';
+import CartPromoCode from './ui/cart/PromoCode.svelte';
+import Warranty from './ui/cart/Warranty.svelte';
+import BtnGroup from './ui/cart/BtnGroup.svelte';
+
+
+export {
+	AddBacketBtn,
+	BurgerMenu,
+	CartInfo,
+	CartSummary,
+	CartPromoCode,
+	UserMenuBtn,
+	Warranty,
+	BtnGroup
+};
